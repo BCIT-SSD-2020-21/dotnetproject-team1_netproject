72f4a9b07ac044e055869b01809f3c665219a8d0
Kalvin Tang <ktang82@my.bcit.ca>
2020-12-08 14:34:53 -0800

    Attempted migration #4

--- Parlez/Startup.cs
+++ Parlez/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Parlez.Data;
 
 namespace Parlez
 {
@@ -25,7 +27,7 @@ namespace Parlez
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddDbContext<ToDoContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ToDoConnection")));
+            services.AddDbContext<ChatDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ChatConnection")));
             services.AddControllers();
         }
 
