81a184635edfb95fad5f31bae2a90863d7eb6416
Kalvin Tang <ktang82@my.bcit.ca>
2020-12-07 22:24:04 -0800

    Initialize Api project. models, data context and seeder #4  #5

---
+++ Parlez/Data/ChatDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parlez.Data
+{
+    public class ChatDbContext : DbContext
+    {
+        public ChatDbContext(DbContextOptions<ChatDbContext> options) : base(options) { }
+
+        public DbSet<Chat> Chats { get; set; }
+    }
+}
--- Parlez/Startup.cs
+++ Parlez/Startup.cs
@@ -25,6 +25,7 @@ namespace Parlez
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //services.AddDbContext<ToDoContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ToDoConnection")));
             services.AddControllers();
         }
 
