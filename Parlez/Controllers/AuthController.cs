2141bb8cbc4a961e5ca3bcd9042700e5285b8469
apurugganan <apurugganan1@my.bcit.ca>
2020-12-08 18:48:12 -0800

    Add AuthCOntroller

---
+++ Parlez/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parlez.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        private IConfiguration _config;
+        private IServiceProvider _serviceProvider;
+
+        public AuthController(SignInManager<IdentityUser> signInManager,
+                                UserManager<IdentityUser> userManager,
+                                IConfiguration config,
+                                IServiceProvider serviceProvider)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _config = config;
+            _serviceProvider = serviceProvider;
+        }
+
+    }
+}
