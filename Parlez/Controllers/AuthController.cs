8db6596ef7cedf3b8bbe27a5ddfb93a91c061d58
apurugganan <apurugganan1@my.bcit.ca>
2020-12-08 18:54:49 -0800

    Add  helper method to AuthController  to generate token

--- Parlez/Controllers/AuthController.cs
+++ Parlez/Controllers/AuthController.cs
@@ -2,9 +2,13 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Parlez.Controllers
@@ -27,6 +31,28 @@ namespace Parlez.Controllers
             _userManager = userManager;
             _config = config;
             _serviceProvider = serviceProvider;
+        }
+
+        string GenerateJSONWebToken(IdentityUser user)
+        {
+            var securityKey
+                = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWT_SITEKEY"]));
+            var credentials
+                = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            var claims = new List<Claim> {
+                            new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+                            new Claim(JwtRegisteredClaimNames.Jti,
+                                        Guid.NewGuid().ToString()),
+                            new Claim(ClaimTypes.NameIdentifier, user.Id)
+             };
+
+            var token = new JwtSecurityToken(
+                    _config["JWT_ISSUER"],
+                    _config["JWT_ISSUER"],
+                    claims,
+                    expires: DateTime.Now.AddMinutes(120),
+                    signingCredentials: credentials);
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
 
     }
