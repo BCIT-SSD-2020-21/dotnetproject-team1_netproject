d07736385a3c766bb3d4fefd37daadc31e6d830c
mandeep1580 <mandeep15@gmail.com>
2020-12-10 16:50:33 -0800

    Did a second migration to test the exception error on the myDelete method on ChatController

--- Parlez/Data/Migrations/20201211004918_TrialV2Schema.Designer.cs
+++ Parlez/Data/Migrations/20201211004918_TrialV2Schema.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Parlez.Data;
 
 namespace Parlez.Data.Migrations
 {
     [DbContext(typeof(ChatDbContext))]
-    partial class ChatDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201211004918_TrialV2Schema")]
+    partial class TrialV2Schema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
