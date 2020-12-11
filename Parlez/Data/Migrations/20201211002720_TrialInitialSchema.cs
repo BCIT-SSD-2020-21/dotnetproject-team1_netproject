83cb84dbd967e401489c31da80d4664cc6321c60
mandeep1580 <mandeep15@gmail.com>
2020-12-10 16:47:44 -0800

    Successfully did migrations to check if data seeds and connects to react.

---
+++ Parlez/Data/Migrations/20201211002720_TrialInitialSchema.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Parlez.Data.Migrations
+{
+    public partial class TrialInitialSchema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedOn",
+                value: new DateTime(2020, 12, 10, 16, 27, 19, 517, DateTimeKind.Local).AddTicks(2567));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedOn",
+                value: new DateTime(2020, 12, 10, 8, 55, 51, 983, DateTimeKind.Local).AddTicks(6857));
+        }
+    }
+}
