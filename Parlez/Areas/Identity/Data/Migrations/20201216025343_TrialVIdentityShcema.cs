7a42e63aa91faad82d1218abbfe00750181b06dd
mandeep1580 <mandeep15@gmail.com>
2020-12-15 18:57:28 -0800

    Merged with main and updated migrations with newShcema for both ChatDbContext and AuthContext

---
+++ Parlez/Areas/Identity/Data/Migrations/20201216025343_TrialVIdentityShcema.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Parlez.Areas.Identity.Data.Migrations
+{
+    public partial class TrialVIdentityShcema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Parlez/Data/Migrations/20201216024935_TrialV3Schema.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Parlez.Data.Migrations
+{
+    public partial class TrialV3Schema : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedOn", "MessageText", "UserName" },
+                values: new object[] { new DateTime(2020, 12, 15, 18, 49, 34, 875, DateTimeKind.Local).AddTicks(6959), "Hello World", "JohnDoe" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedOn", "MessageText", "UserName" },
+                values: new object[] { new DateTime(2020, 12, 10, 16, 49, 17, 409, DateTimeKind.Local).AddTicks(7694), "Clean house", "suup" });
+        }
+    }
+}
